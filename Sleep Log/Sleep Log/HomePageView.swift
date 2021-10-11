e6396efb42a7befbea41a2710c318f15b34e9311
Aristotle Loridas <91709825+Stotle510@users.noreply.github.com>
2021-10-11 16:33:52 -0400

    Add files via upload

---
+++ Sleep Log/Sleep Log/HomePageView.swift
@@ -0,0 +1,149 @@
+//
+//  HomePageView.swift
+//  Sleep Log
+//
+//  Created by James on 10/7/21.
+//
+
+import SwiftUI
+
+struct HomePageView: View {
+    var body: some View {
+        ZStack{
+            
+            Image("background").ignoresSafeArea()
+            
+            VStack{
+                
+                Text("My Daily Log:")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundColor(Color.blue)
+                
+                Text(" ")
+                    .padding(.all, 10.0)
+                
+                VStack{
+                    HStack{
+                        Button(action: {}, label: {
+                            Image("sun").resizable().frame(width: 100, height:100)
+                        })
+                        Text("  ")
+                        Button(action: {}, label: {
+                            Image("zzz").resizable().frame(width: 100, height:100)
+                        })
+                        Text("  ")
+                        Button(action: {}, label: {
+                            Image("moon").resizable().frame(width: 100, height:100)
+                        })
+                    }
+                    
+                    HStack{
+                        VStack{
+                            Text("Wake Up")
+                                .font(.title3)
+                                .fontWeight(.bold)
+                            Text("Questions")
+                                .font(.title3)
+                                .fontWeight(.bold)
+                        }
+                        VStack{
+                            Text("Nap")
+                                .font(.title3)
+                                .fontWeight(.bold)
+                            Text("Questions")
+                                .font(.title3)
+                                .fontWeight(.bold)
+                        }
+                        .frame(width: 140.0)
+                        
+                        VStack{
+                            Text("BedTime")
+                                .font(.title3)
+                                .fontWeight(.bold)
+                            Text("Questions")
+                                .font(.title3)
+                                .fontWeight(.bold)
+                        }
+                    }
+                    
+                    Text(" ")
+                        .padding(.all, 5.0)
+                    
+                    ZStack{
+                        Button(action: {}, label: {
+                            Image("button").resizable().frame(width:350, height:75)
+                        })
+                        Text("Start Light Vision Timer")
+                            .font(.title2)
+                            .fontWeight(.black)
+                            .foregroundColor(Color.white)
+                            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    }
+                    
+                    Text(" ")
+                        .padding(.all, 5.0)
+                    
+                    HStack{
+                        Button(action: {}, label: {
+                            Image("bell").resizable().frame(width: 100, height:100)
+                        })
+                        Text("  ")
+                        Button(action: {}, label: {
+                            Image("info").resizable().frame(width: 100, height:100)
+                        })
+                        Text("  ")
+                        Button(action: {}, label: {
+                            Image("question").resizable().frame(width: 100, height:100)
+                        })
+                    }
+                    
+                    HStack{
+                        VStack{
+                            Text("Set")
+                                .font(.title3)
+                                .fontWeight(.bold)
+                            Text("Reminders")
+                                .font(.title3)
+                                .fontWeight(.bold)
+                        }
+                        VStack{
+                            Text("My Info")
+                                .font(.title3)
+                                .fontWeight(.bold)
+                            Text("")
+                        }
+                        .frame(width: 132.0)
+                        VStack{
+                            Text("Help")
+                                .font(.title3)
+                                .fontWeight(.bold)
+                            Text("                       ")
+                        }
+                    }
+                }
+                
+                
+                Text(" ")
+                    .padding(.all, 10.0)
+                
+                ZStack{
+                    Button(action: {}, label: {
+                        Image("bluebutton").resizable().frame(width: 120, height:50)
+                    })
+                    Text("Log Out")
+                        .font(.title2)
+                        .fontWeight(.black)
+                        .foregroundColor(Color.white)
+                        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                }
+            }
+        }
+    }
+}
+
+struct HomePageView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomePageView()
+    }
+}
