57c8c20fae2b4f65ffe6cfa4a60840b8d015af44
Aristotle Loridas <91709825+Stotle510@users.noreply.github.com>
2021-10-11 16:28:48 -0400

    Add files via upload

---
+++ AppCode/Sleep Log/Sleep Log/Sleep_LogApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sleep_LogApp.swift
+//  Sleep Log
+//
+//  Created by James on 10/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Sleep_LogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
