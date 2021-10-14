ec797fd2c09d36332dd401d75c765eea6c6eba48
atwainhu <atwainhu@msu.edu>
2021-10-14 12:00:34 -0400

    Testing the Commit

---
+++ Sleep Log/Sleep Log/WakUpView.swift
@@ -0,0 +1,20 @@
+//
+//  WakUpView.swift
+//  Sleep Log
+//
+//  Created by Hussain Atwain on 10/14/21.
+//
+
+import SwiftUI
+
+struct WakUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WakUpView_Previews: PreviewProvider {
+    static var previews: some View {
+        WakUpView()
+    }
+}
