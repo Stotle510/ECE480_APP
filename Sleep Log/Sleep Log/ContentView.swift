68b8bf75f79a34fe72740e606837a3c19e17a5a8
Aristotle Loridas <loridas2@msu.edu>
2021-10-18 00:21:48 -0400

    Log-in screen update

--- Sleep Log/Sleep Log/ContentView.swift
+++ Sleep Log/Sleep Log/ContentView.swift
@@ -8,33 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var username: String = ""
-    @State private var password: String = ""
-    
-    var admin_username: String = "loridasa"
-    var admin_password: String = "arilor"
-    
+    @State var email = ""
+    @State var password = ""
     var body: some View {
         NavigationView {
-            VStack{
-                Text("Log-In: enter username")
-                    .padding()
-                    .frame(width: 500.0, height: 200.0)
-                    .font(.largeTitle)
-                    .foregroundColor(Color.blue)
+            VStack(spacing: 15){
                 Spacer()
-                NavigationLink(destination: HomePageView()            .navigationBarBackButtonHidden(true)
+                Spacer()
+                Text("Log-In")
+                    .font(.system(size: 65, weight: .bold))
+                    .foregroundColor(.white)
+                HStack{
+                    Image(systemName: "envelope")
+                        .foregroundColor(.gray)
+                    TextField("Username", text: $email)
+                }
+                    .padding(.all, 20)
+                    .background(Color.white)
+                    .cornerRadius(8)
+                    .padding(.horizontal, 20)
+                HStack{
+                    Image(systemName: "lock")
+                        .foregroundColor(.gray)
+                    SecureField("Password", text: $password)
+                }
+                    .padding(.all, 20)
+                    .background(Color.white)
+                    .cornerRadius(8)
+                    .padding(.horizontal, 20)
+                NavigationLink(destination: HomePageView()          .navigationBarBackButtonHidden(true)
                     .navigationBarBackButtonHidden(true)
                     .navigationBarHidden(true)) {
                     Text("Continue To Home Screen")
-                        .padding()
-                        .frame(width: 200.0, height: 200.0)
-                        .foregroundColor(.pink)
-                        .cornerRadius(40)
+                        .padding(.vertical, 20)
+                        .frame(width: 370.0, height: 60.0)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
                         .font(.title)
                 }
-            }
-            .edgesIgnoringSafeArea([.top, .bottom])
+                Spacer()
+                Spacer()
+            }.background(
+                Image("logbackground")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+            ).edgesIgnoringSafeArea(.all)
         }
     }
 }
